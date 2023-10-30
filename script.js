493817d6af524026e8dc552deed33f1c2682b59c
umaar-kr <umaar.kr@gmail.com>
2023-10-30 18:47:22 +0900

    smooth scrolling in Page Navigation

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+
+//////////////////////////////////
+////////// Modal window //////////
+//////////////////////////////////
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +23,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,28 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+////////////////////////////////
+////////// Scrolling //////////
+///////////////////////////////
+
+btnScrollTo.addEventListener('click', function() {
+  section1.scrollIntoView({behavior: 'smooth'});
+});
+
+
+////////////////////////////////
+////////// Page Navigation /////
+///////////////////////////////
+
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  e.preventDefault();
+
+  if(e.target.classList.contains('nav__link')){
+    const id = e.target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
